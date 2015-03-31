35a190d8258050b38ac5e4c0e163ee67c572fbd1
William Stevens <wastevensv@gmail.com>
2015-03-31 13:46:51 -0400

    Working.
    14.7s. Cutting it close. But working.

--- src/org/usfirst/frc/team61/robot/commands/AutonomousThreeStack.java
+++ src/org/usfirst/frc/team61/robot/commands/AutonomousThreeStack.java
@@ -8,15 +8,15 @@ public class AutonomousThreeStack extends CommandGroup {
 			addSequential(new BucketLift());
 			addParallel(new ElevWithEncoder(30,.7));
 			addSequential(new WaitForTime(.3));
-			addSequential(new TurnForDegrees(162,.4));
-			addSequential(new DriveForDistance(20,-.3)); // Step 2 // Tweak up?
+			addSequential(new TurnForDegrees(155,.45));
+			addSequential(new DriveForDistance(20,-.4)); // Step 2 // Tweak up?
 			addSequential(new ClawToggle());
-			addSequential(new DriveForDistance(20,-.3)); // Tweak up?
-			addSequential(new TurnForDegrees(-157, .4)); // Step 3 // Needs to be tweaked.
+			addSequential(new DriveForDistance(20,-.4)); // Tweak up?
+			addSequential(new TurnForDegrees(-145, .45)); // Step 3 // Needs to be tweaked.
 			addSequential(new BucketLift());
 			addSequential(new DriveForDistance(55,-.7));
 			addSequential(new ClawSetAuto(0)); // Step 4 - DROP
-			addSequential(new ElevWithEncoder(10,.7));
+			addSequential(new ElevWithEncoder(10,.7)); // Tweak up?
 			addSequential(new ClawToggle()); // Step 5
 			addSequential(new ReverseDriveForDistance(5,-.3));
 			addSequential(new ElevWithEncoder(0,.8));
@@ -25,10 +25,10 @@ public class AutonomousThreeStack extends CommandGroup {
 			addSequential(new ClawSetAuto(1));
 			addParallel(new ElevWithEncoder(30,.7)); // LIFT
 			addSequential(new ClawToggle());
-			addSequential(new DriveForDistance(55,-.7));
-			addSequential(new DriveForDistance(20,-.4));
+			addSequential(new DriveForDistance(52,-.7));
+			addSequential(new DriveForDistance(15,-.4));
 			addSequential(new ClawToggle());
-			addSequential(new TurnForDegrees(85,.3)); // Step 7
+			addSequential(new TurnForDegrees(85,.4)); // Step 7 // Slow down??
 			addSequential(new DriveForDistance(80,-.8));
 			addSequential(new DriveForDistance(16,-.4));
 			addSequential(new ClawSetAuto(0));
