8bc2c26a6fba0292fc3580d2eafddfc450b3bc2e
William Stevens <wastevensv@gmail.com>
2015-04-01 13:35:35 -0400

    Three tote stack in 14s.

--- src/org/usfirst/frc/team61/robot/commands/AutonomousThreeStack.java
+++ src/org/usfirst/frc/team61/robot/commands/AutonomousThreeStack.java
@@ -12,26 +12,23 @@ public class AutonomousThreeStack extends CommandGroup {
 			addSequential(new DriveForDistance(20,-.4)); // Step 2 // Tweak up?
 			addSequential(new ClawToggle());
 			addSequential(new DriveForDistance(20,-.4)); // Tweak up?
-			addSequential(new TurnForDegrees(-145, .45)); // Step 3 // Needs to be tweaked.
+			addSequential(new TurnForDegrees(-140, .45)); // Step 3 // Needs to be tweaked.
 			addSequential(new BucketLift());
 			addSequential(new DriveForDistance(55,-.7));
-			addSequential(new ClawSetAuto(0)); // Step 4 - DROP
 			addSequential(new ElevWithEncoder(10,.7)); // Tweak up?
 			addSequential(new ClawToggle()); // Step 5
 			addSequential(new ReverseDriveForDistance(5,-.3));
 			addSequential(new ElevWithEncoder(0,.8));
 			addSequential(new DriveForDistance(10,-.3)); // Step 6
 			addSequential(new ClawToggle());
-			addSequential(new ClawSetAuto(1));
 			addParallel(new ElevWithEncoder(30,.7)); // LIFT
 			addSequential(new ClawToggle());
-			addSequential(new DriveForDistance(52,-.7));
-			addSequential(new DriveForDistance(15,-.4));
+			addSequential(new DriveForDistance(55,-.7)); //Changed from 67
 			addSequential(new ClawToggle());
-			addSequential(new TurnForDegrees(85,.4)); // Step 7 // Slow down??
-			addSequential(new DriveForDistance(80,-.8));
-			addSequential(new DriveForDistance(16,-.4));
-			addSequential(new ClawSetAuto(0));
+			addSequential(new WaitForTime(.5));
+			addSequential(new TurnForDegrees(70,.5)); // Step 7
+			addSequential(new DriveForDistance(75,-.8));
+			addSequential(new DriveForDistance(15,-.4));
 			addSequential(new ElevWithEncoder(12,.6));
 			addSequential(new ClawToggle());
 			addSequential(new ReverseDriveForDistance(10,-.3));
