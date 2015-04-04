fc4441a58a3820624c16f6ace28713c6196649e3
William Stevens <wastevensv@gmail.com>
2015-04-04 12:29:28 -0400

    Replaced AutoOneTote with v2.2.1 copy.
    Found in blinkenlights branch. AutoThreeTote still needs tweaking.

--- src/org/usfirst/frc/team61/robot/commands/AutoOneTote.java
+++ src/org/usfirst/frc/team61/robot/commands/AutoOneTote.java
@@ -20,12 +20,15 @@ public class AutoOneTote extends CommandGroup {
     	addSequential(new ClawToggle());
     	addSequential(new ClawSetAuto(1));
     	addSequential(new TurnForDegrees(-80, 0.3));
-    	addSequential(new DriveForDistance(136, -.4)); // Try 134 or 132 next.
+    	addSequential(new DriveForDistance(136, -.45)); // Try 134 or 132 next.
     	addSequential(new WaitForTime(0.75));
     	addSequential(new TurnForDegrees(-80, 0.3));
     	addSequential(new ClawToggle());
-    	addSequential(new ReverseDriveForDistance(15, -0.4));
-    	addSequential(new TurnForDegrees(160, 0.3));
+    	addSequential(new WaitForTime(0.5));
+    	addSequential(new ReverseDriveForDistance(20, -0.4));
+    	addSequential(new ClawToggle());
+    	addSequential(new TurnForDegrees(-160, 0.3));
+    	addSequential(new ClawToggle());
     }
 
 }
